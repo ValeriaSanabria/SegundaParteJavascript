a50d3e7938e8674515aafc50e12945c1ba29f7cc
Valeria Sanabria <valeriasanabria@tdf.edu.ar>
2022-12-11 12:38:12 -0300

    calculadora

--- 22-11-21/calculadora/script.js
+++ 22-11-21/calculadora/script.js
@@ -15,16 +15,15 @@
     document.querySelector('#calcular').addEventListener('click', () =>{
         const n1 = parseInt(document.querySelector('#n1').value);
         const n2 = parseInt(document.querySelector('#n2').value);
-        //const op = document.querySelector('#op').value;
-        let r;
+        let respuesta;
         if(op == '+'){
-            r = n1 + n2;
+            respuesta = n1 + n2;
         }else if( op == '-'){
-            r = n1 - n2;
+            respuesta = n1 - n2;
         }else if(op == '*'){
-            r = n1 * n2;
+            respuesta = n1 * n2;
         }else if(op == '/'){
-            r = n1 / n2;
+            respuesta = n1 / n2;
         }
-        document.querySelector('#r').innerHTML = r;
+        document.querySelector('#respuesta').innerHTML = respuesta;
     });
